0c4f820ee1f1d9d775267b23b970e82d28b51038
Brandon <167170288+BrandonVH@users.noreply.github.com>
2025-11-16 18:16:38 -0800

    scroll scatterplot

--- meta/main.js
+++ meta/main.js
@@ -23,7 +23,7 @@ async function loadData() {
 // Extract data about commits in a separate variable for easy access. We will compute this after reading the CSV file. First, we’ll use the d3.groups() method to group the data by the commit property
 function processCommits(data) { 
   return d3
-    .groups(data, (d) => d.commit)
+    .groups(data, (d) => d.commit)  
     .map(([commit, lines]) => {
       let first = lines[0];
       let { author, date, time, timezone, datetime } = first;
@@ -47,7 +47,8 @@ function processCommits(data) {
       });
 
       return ret;
-    });
+    })
+    .sort((a, b) => a.datetime - b.datetime); // sort commits by datetime to update the scatter plot properly for scrolling feature
 }
 
 // display the total lines of code and commits
@@ -111,7 +112,7 @@ function renderScatterPlot(data, commits) {
 
     dots
         .selectAll('circle')
-        .data(sortedCommits)
+        .data(sortedCommits, (d) => d.id)
         .join('circle')
         .attr('cx', (d) => xScale(d.datetime))
         .attr('cy', (d) => yScale(d.hourFrac))
@@ -162,12 +163,14 @@ function renderScatterPlot(data, commits) {
     // Add X axis
     svg
         .append('g')
+        .attr('class', 'x-axis')
         .attr('transform', `translate(0, ${usableArea.bottom})`)
         .call(xAxis);
 
     // Add Y axis
     svg
         .append('g')
+        .attr('class', 'y-axis')
         .attr('transform', `translate(${usableArea.left}, 0)`)
         .call(yAxis);
 
@@ -309,3 +312,210 @@ let yScale = d3.scaleLinear().domain([0, 24]).range([600, 0]); // standard linea
 
 renderCommitInfo(data, commits);
 renderScatterPlot(data, commits);
+
+
+
+// FOR INTERACTIVE TIMELINE VISUALIZATION ----------------------------------------------------------------------------------
+let commitProgress = 100; // represent the maximum time we want to show as a percentage of the total time:
+let timeScale = d3 // time scale to map the percentage to a date
+  .scaleTime()
+  .domain([
+    d3.min(commits, (d) => d.datetime),
+    d3.max(commits, (d) => d.datetime),
+  ])
+  .range([0, 100]);
+let commitMaxTime = timeScale.invert(commitProgress);
+const slider = document.querySelector("#commit-progress");
+const timeEl = document.querySelector("#commit-time");
+let filteredCommits = commits;
+
+// We want to display the file details for the commits we filtered. Obtain the file names and lines associated with each file
+function updateFileDisplay(filteredCommits) {
+  let lines = filteredCommits.flatMap(d => d.lines);
+  let files = d3.groups(lines, d => d.file)
+                .map(([name, lines]) => ({ name, lines }))
+                .sort((a, b) => b.lines.length - a.lines.length);
+
+  let filesContainer = d3
+    .select('#files')
+    .selectAll('div')
+    .data(files, d => d.name)
+    .join(
+      enter => enter.append('div').call(div => {
+        const dt = div.append('dt');
+        dt.append('code');
+        dt.append('small');        // add the line-count element
+        div.append('dd');          // for unit viz
+      })
+    );
+
+  // put filename + line count into the <dt>
+  filesContainer
+    .select('dt')
+    .html(d => `<code>${d.name}</code><small>${d.lines.length} lines</small>`);
+
+  // create ordinal scale to map technology id to colors
+  let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+  // unit visualization inside <dd>
+  filesContainer
+    .select('dd')
+    .selectAll('div')
+    .data(d => d.lines)
+    .join('div')
+    .attr('class', 'loc')
+    .attr('style', (d) => `--color: ${colors(d.type)}`);
+}
+
+// UPDATE SCATTER PLOT BASED ON FILTER
+function updateScatterPlot(data, commits) {
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  const svg = d3.select('#chart').select('svg');
+
+  xScale = xScale.domain(d3.extent(commits, (d) => d.datetime));
+
+  const [minLines, maxLines] = d3.extent(commits, (d) => d.totalLines);
+  const rScale = d3.scaleSqrt().domain([minLines, maxLines]).range([2, 30]);
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+  // CHANGE: we should clear out the existing xAxis and then create a new one.
+  const xAxisGroup = svg.select('g.x-axis');
+  xAxisGroup.selectAll('*').remove();
+  xAxisGroup.call(xAxis);
+  const yAxisGroup = svg.select('g.y-axis');
+  yAxisGroup.selectAll('*').remove();
+  yAxisGroup.call(yAxis);
+  svg.select('.x-axis').call(xAxis);
+  svg.select('.y-axis').call(yAxis);
+
+  const dots = svg.select('g.dots');
+
+  const sortedCommits = d3.sort(commits, (d) => -d.totalLines);
+  dots
+    .selectAll('circle')
+    .data(sortedCommits, (d) => d.id) // When we move the slider, circles jump around a lot. This is because D3 doesn’t know which data items correspond to which previous data items, so it does not necessarily reuse the right <circle> element for the same commit. To tell D3 which data items correspond to which previous data items, we can give each circle a key that uniquely identifies the data item. A good candidate for that in this case would be the commit id.
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', (d) => rScale(d.totalLines))
+    .attr('fill', 'steelblue')
+    .style('fill-opacity', 0.7) // Add transparency for overlapping dots
+    .on('mouseenter', (event, commit) => {
+      d3.select(event.currentTarget).style('fill-opacity', 1); // Full opacity on hover
+      renderTooltipContent(commit);
+      updateTooltipVisibility(true);
+      updateTooltipPosition(event);
+    })
+    .on('mouseleave', (event) => {
+      d3.select(event.currentTarget).style('fill-opacity', 0.7);
+      updateTooltipVisibility(false);
+    });
+}
+
+function onTimeSliderChange() { // update slider and textContent of time element that displays time
+    commitProgress = +slider.value; // Update the commitProgress variable to the slider value.
+    commitMaxTime = timeScale.invert(commitProgress); // Update the commitMaxTime variable to the date corresponding to the slider value using d3.invert().
+    timeEl.textContent = commitMaxTime.toLocaleString();
+    filteredCommits = commits.filter((d) => d.datetime <= commitMaxTime);
+    updateScatterPlot(data, filteredCommits);
+    updateFileDisplay(filteredCommits)
+}
+slider.addEventListener("input", onTimeSliderChange);
+onTimeSliderChange();
+
+// Generate some filler text for each commit.
+d3.select('#scatter-story')
+  .selectAll('.step')
+  .data(commits)
+  .join('div')
+  .attr('class', 'step')
+  .html(
+    (d, i) => `
+		On ${d.datetime.toLocaleString('en', {
+      dateStyle: 'full',
+      timeStyle: 'short',
+    })},
+		I made <a href="${d.url}" target="_blank">${
+      i > 0 ? 'another glorious commit' : 'my first commit, and it was glorious'
+    }</a>.
+		I edited ${d.totalLines} lines across ${
+      d3.rollups(
+        d.lines,
+        (D) => D.length,
+        (d) => d.file,
+      ).length
+    } files.
+		Then I looked over all I had made, and I saw that it was very good.
+	`,
+  );
+
+// Use Scrollama to automatically update our scatter plot as we scroll past commits
+import scrollama from 'https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm';
+
+// Add this to the bottom of your JS file to use the library
+function onStepEnter(response) {
+  const commit = d3.select(response.element).datum();   // 🔥 original commit data
+  
+  // make sure commits are sorted by datetime in processCommits()
+  const idx = commits.indexOf(commit);
+
+  const filtered = commits.slice(0, idx + 1);
+
+  updateScatterPlot(data, filtered);
+
+  // console.log(commit);    
+  // console.log(commit.date);
+  // console.log(response.element.__data__.datetime);
+}
+const scroller = scrollama();
+scroller
+  .setup({
+    container: '#scrolly-1',
+    step: '#scrolly-1 .step',
+  })
+  .onStepEnter(onStepEnter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
